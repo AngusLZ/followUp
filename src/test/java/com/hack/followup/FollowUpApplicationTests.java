4129c2a0507ebc60056dff310c8021ed1793eee9
hack <942848439@qq.com>
2021-04-20 14:14:49 +0800

    init

---
+++ src/test/java/com/hack/followup/FollowUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.followup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
